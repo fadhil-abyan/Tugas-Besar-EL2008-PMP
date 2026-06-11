b4c4788d9b373e8ec91972276b7192bd1363c5cf
dimsrist <dimasristanto1@gmail.com>
2026-06-12 06:58:46 +0700

    Update tambahData.c

--- tambahData.c
+++ tambahData.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include "inventaris.h"
 
+void tambahData(Node** head, Item item) {
+  // Pengecekan ID duplikat
+  Node* current = *head;
+  
+  while (current != NULL) {
+    if (strcmp(current->data.id, item.id) == 0 {
+      Serial.println("Gagal: ID barang sudah terdaftar!");
+      return;
+    }
+    current = current->next;
+  }
+
+  // Penanganan Kapasitas Memori Habis
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  
+  if (newnode == NULL) {
+    serial.println("Gagal: Kapasitas memori tidak mencukupi!");
+    return;
+  }
+
+  // Penambahan Data Ke Node Baru
+  newNode->data = item;
+  newNode->next = NULL;
+  
+  if (*head == NULL) {
+    *head = newNode;
+  } else {
+    curent = *head;
+    while (current->next != NULL) {
+      current = current->next;
+    }
+    current->next = newNode;
+  }
+  Serial.println("Sukses: Data berhasil ditambahkan!");
+}
