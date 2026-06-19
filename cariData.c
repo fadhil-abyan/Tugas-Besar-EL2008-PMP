e465b55d16b921ab1d79ad050e59dfd4bbb7ea3e
dimsrist <dimasristanto1@gmail.com>
2026-06-19 13:06:04 +0700

    Update cariData.c

--- cariData.c
+++ cariData.c
@@ -8,23 +8,23 @@ void cariData(Node* head, char* id) {
   Node* current = head;
 
   if (current == NULL) {
-    Serial.println("Inventaris kosong. Tidak ada data yang dapat dicari!");
+    printf("Inventaris kosong. Tidak ada data yang dapat dicari!");
     return;
   }
 
   while (current != NULL) {
     if (strcmp(current->data.id, id) == 0) {
-      Serial.print("ID: "); Serial.println(current->data.id);
-      Serial.print("Nama: "); Serial.println(current->data.nama);
-      Serial.print("Kategori: "); Serial.println(current->data.kategori);
-      Serial.print("Stok: "); Serial.println(current->data.stok);
-      Serial.print("Lokasi: "); Serial.println(current->data.lokasi);
-      Serial.print("Status: "); Serial.println(current->data.status);
-      Serial.print("Pemilik: "); Serial.println(current->data.pemilik);
-      Serial.print("PIC: "); Serial.println(current->data.pic);
+      print("ID: %s\n", current->data.id);
+      print("Nama: %s\n", current->data.nama);
+      print("Kategori: %s\n", current->data.kategori);
+      print("Stok: %d\n", current->data.stok);
+      print("Lokasi: %s\n", current->data.lokasi);
+      print("Status: %s\n", current->data.status);
+      print("Pemilik: %s\n", current->data.pemilik);
+      print("PIC: %s\n", current->data.pic);
       return;
     }
     current = current->next;
   }
-  Serial.println("Pencarian Gagal: ID tidak ditemukan di dalam sistem!");
+  printf("Pencarian Gagal: ID tidak ditemukan di dalam sistem!");
 }
